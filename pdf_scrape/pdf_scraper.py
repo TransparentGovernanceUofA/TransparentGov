e2956e736235e880b14810107f1dd58eba412a91
AustinGrey <corey.crapo@gmail.com>
2018-02-10 12:35:23 -0700

    Much improved PDF scrape

--- pdf_scrape/pdf_scraper.py
+++ pdf_scrape/pdf_scraper.py
@@ -1,5 +1,36 @@
+'''
 import pdfquery
 pdf = pdfquery.PDFQuery("pdfs/Combined-agenda-and-documents.pdf")
 pdf.load()
 
 pdf.tree.write('test.xml', pretty_print=True)
+'''
+# Adapted from https://reachtim.com/articles/PDF-Manipulation.html
+# Updated to python 3 by Austin Crapo
+from io import StringIO
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+
+def convert(fname, pages=None):
+	if not pages:
+		pagenums = set()
+	else:
+		pagenums = set(pages)
+
+	output = StringIO()
+	manager = PDFResourceManager()
+	converter = TextConverter(manager, output, laparams=LAParams())
+	interpreter = PDFPageInterpreter(manager, converter)
+
+	infile = open(fname, 'rb')
+	for page in PDFPage.get_pages(infile, pagenums):
+		interpreter.process_page(page)
+	infile.close()
+	converter.close()
+	text = output.getvalue()
+	output.close
+	return text
+	
+print(convert('pdfs/Combined-agenda-and-documents.pdf', pages=[5]))
