70c94a2eb6617c46b25526ba615a987542efe621
Aquafina99 <xxiang1@ualberta.ca>
2018-02-13 20:19:28 -0700

    Defining Templates and URLs

--- transgov/src/searchapp/urls.py
+++ transgov/src/searchapp/urls.py
@@ -15,9 +15,15 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from main_search.views import home
+from main_search.views import HomeView, MeetingView, FacetedSearchView, autocomplete
+from .settings import MEDIA_ROOT, MEDIA_URL
+from django.conf.urls.static import static
 
 urlpatterns = [
+    url(r'^$', HomeView.as_view()),
     url(r'^admin/', admin.site.urls),
-    url(r'^$', home),
-]
+    url(r'^meeting/(?P<slug>[\w-]+)/$', MeetingView.as_view(), name='meeting'),
+    url(r'^search/autocomplete/$', autocomplete),
+    url(r'^find/', FacetedSearchView.as_view(), name='haystack_search'),
+
+] + static(MEDIA_URL, document_root=MEDIA_ROOT)
