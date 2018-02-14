9c728d84e8b5e9b72143d76956b9685bb89c87de
Aquafina99 <xxiang1@ualberta.ca>
2018-02-13 20:18:03 -0700

    Adding Views and Forms

--- transgov/src/main_search/views.py
+++ transgov/src/main_search/views.py
@@ -1,7 +1,40 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.views.generic.detail import DetailView
+from django.http import JsonResponse
+from haystack.generic_views import FacetedSearchView as BaseFacetedSearchView
+from haystack.query import SearchQuerySet
 
-# Create your views here.
-def home(request):
-    return HttpResponse("Hello")
-    #return render(request, "home.html", {})#response
+from .models import Meeting
+from .forms import FacetedMeetingSearchForm
+
+
+class HomeView(TemplateView):
+    template_name = "home.html"
+
+
+class MeetingView(DetailView):
+    template_name = "meeting.html"
+    model = Meeting
+
+
+def autocomplete(request):
+    sqs = SearchQuerySet().autocomplete(
+        content_auto=request.GET.get(
+            'query',
+            ''))[
+        :5]
+    s = []
+    for result in sqs:
+        d = {"value": result.title, "data": result.object.slug}
+        s.append(d)
+    output = {'suggestions': s}
+    return JsonResponse(output)
+
+
+class FacetedSearchView(BaseFacetedSearchView):
+
+    form_class = FacetedMeetingSearchForm
+    facet_fields = ['category', 'committee']
+    template_name = 'search_result.html'
+    paginate_by = 3
+    context_object_name = 'object_list'
