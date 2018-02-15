2d875d737d55a7a1960a708186e88c653cd51e4c
VukR <vuk@ualberta.ca>
2018-02-15 16:50:37 -0700

    attempting axios

--- prototype/src/components/TopLeftSearch.vue
+++ prototype/src/components/TopLeftSearch.vue
@@ -12,10 +12,36 @@
 
 <script>
 import SearchResultList from './SearchResultList.vue'
+import axios from 'axios'
 
 export default{
   components: {
     SearchResultList
+  },
+  name: 'ElasticResults',
+  data () {
+    return {
+      ElasticResult: {}
+    }
+  },
+  created () {
+    this.fetchData()
+  },
+
+  watch: {
+    '$route': 'fetchData'
+  },
+  methods: {
+    fetchData () {
+      axios.get('http://162.246.156.217:9200/meeting_minutes')
+        .then((resp) => {
+          this.ElasticResult = resp.data[0]
+          console.log(resp)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    }
   }
 }
 
