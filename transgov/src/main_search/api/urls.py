ede0c5ce0cd4fa7237009f33d064b8f6971efc0f
Aquafina99 <xxiang1@ualberta.ca>
2018-02-25 04:08:55 -0700

    Django API Testing with 8 testcases

--- transgov/src/main_search/api/urls.py
+++ transgov/src/main_search/api/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from .views import MeetingRUDView, MeetingAPIView
 
 urlpatterns = [
-    url(r'^$', MeetingAPIView.as_view(), name='meeting-create'),
+    url(r'^$', MeetingAPIView.as_view(), name='meeting-listcreate'),
     url(r'^(?P<pk>\d+)/$', MeetingRUDView.as_view(), name='meeting-rud'),
 ]
