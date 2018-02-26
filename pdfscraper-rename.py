544433dce3403b193685768330dc1435de09a038
gsgould <gsgould@ualberta.ca>
2018-02-26 10:16:31 -0700

    PDF Scrape now renames files creates subdirectories for each portion of text for indexing.

--- pdfscraper-rename.py
+++ pdfscraper-rename.py
@@ -26,37 +26,61 @@ def convert(fname, pages=None):
 	converter = TextConverter(manager, output, laparams=LAParams())
 	interpreter = PDFPageInterpreter(manager, converter)
 
-	infile = open(fname, 'rb')
-	for page in PDFPage.get_pages(infile, pagenums):
-		interpreter.process_page(page)
-	infile.close()
-	converter.close()
-	text = output.getvalue()
-	output.close
+	with open(fname, 'rb') as infile:
+		for page in PDFPage.get_pages(infile, pagenums):
+			interpreter.process_page(page)
+		infile.close()
+		converter.close()
+		text = output.getvalue()
+		output.close
 	
 	return text
 
 
-def parse(fileName):
+def parse(fileName, filePointers):
+	print(filePointers)
 	lineCount = 0
 	pageCount = 0
 	i= 0
-	with open(fileName,'rb') as file:
-		for text in file:
-			# Line Count to show progress
-			lineCount = lineCount + 1
-			if (lineCount % 23 == 0): #23 lines/page at 12pt font
-				pageCount = pageCount + 1
-				print("Pages Printed: "+str(pageCount))			
-			# Read lines
-			if keywords[i] in text:
-				i = i + 1
-				print("i: "+str(i-1)+"|"+str(keywords[i-1]))
-			if i > 0:
-				filePointers[i-1].write(text)	
+	file = open(fileName,'rb')
+	date = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+	for text in file:
+		# Line Count to show progress
+		lineCount = lineCount + 1
+		if lineCount < 10:
+			for day in date:
+				if day in str(text):
+					newName = str(text)
+					print("Date: " + newName)
+					print("Renaming")
+					newName = newName.split(", ",1)
+					print(newName)
+					newName = newName[1]
+					newName = newName[:-4]					
+					
+					pdfName = fileName.split(".",1)
+					pdfName = pdfName[0] 
+					print("pdfName: " + pdfName)
+					
+					print("newName: " + path + newName)
+					#os.rename(pdfName + ".pdf", path + "/Approved_Minutes " + newName + ".pdf")
+					#os.rename(pdfName + ".txt", path + "/Approved_Minutes " + newName + ".txt")
+					
+		if (lineCount % 23 == 0): #23 lines/page at 12pt font
+			pageCount = pageCount + 1
+			print("Pages Printed: "+str(pageCount))			
+		# Read lines
+		if keywords[i] in text:
+			i = i + 1
+			#print("i: "+str(i-1)+"|"+str(keywords[i-1]))
+		if i > 0:
+			#print("Prewrite i: " + str(i-1))
+			#print(filePointers)
+			filePointers[i-1].write(text)	
 				
 				
 def processFile(fileName):
+	filePointers = []
 	fileName = fileName.split(".",1)
 	fileName = fileName[0]
 	output = convert(fileName + ".pdf")
@@ -76,12 +100,11 @@ def processFile(fileName):
 		filePointers.append(fp)
 		#print(str(splitNames[i])+" Pointer i: "+str(i))
 		i = i + 1
+		
 	
 	keywords.append(splitStrings[0].encode('utf-8')) #Add an extra
 	
-	parse(path + "/" + fileName + ".txt")
-	
-	
+	parse(path + "/" + fileName + ".txt", filePointers)
 				
 	# Close files
 	for file in filePointers:
@@ -89,6 +112,9 @@ def processFile(fileName):
 		file.close()
 	print()	
 	print(fileName + " is finished being processed")
+	
+	
+		
 	
 def isPDF(file):
 	file1 = file.split(".",1)
@@ -106,7 +132,7 @@ def isPDF(file):
 #####################################
 
 # Define deliniators 
-	
+newName = ""
 keywords = []
 filePointers = []
 
