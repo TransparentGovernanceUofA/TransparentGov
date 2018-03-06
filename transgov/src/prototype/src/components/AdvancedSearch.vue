5bceabc7ed752f2c3f03ae3349b898a9b90e19ec
VukR <vuk@ualberta.ca>
2018-03-06 14:06:38 -0700

    cleaning up

--- transgov/src/prototype/src/components/AdvancedSearch.vue
+++ transgov/src/prototype/src/components/AdvancedSearch.vue
@@ -1,62 +1,62 @@
 <template>
   <div class="advancedSearch">
     <top-left-search></top-left-search>
-      <div id="AdvancedSearch">
-        <!-- <center>
-            ADVANCED COMPONENT STUFF HERE
-        </center> -->
+    <div id="AdvancedSearch">
+      <!-- <center>
+          ADVANCED COMPONENT STUFF HERE
+      </center> -->
 
-        <b-form-group id="topics"
-                    label="Topics"
-                    label-for="exampleInput1">
-          <b-form-select id="exampleInput1"
-                      :options="topicOptions"
-                      required
-                      v-model="form.topic">
-          </b-form-select>
-        </b-form-group>
+      <b-form-group id="topics"
+                  label="Topics"
+                  label-for="exampleInput1">
+        <b-form-select id="exampleInput1"
+                    :options="topicOptions"
+                    required
+                    v-model="form.topic">
+        </b-form-select>
+      </b-form-group>
 
-        <b-form-group id="committee"
-                    label="Committee"
-                    label-for="exampleInput2">
-          <b-form-select id="exampleInput2"
-                      :options="committeeOptions"
-                      required
-                      v-model="form.committee">
-          </b-form-select>
-        </b-form-group>
+      <b-form-group id="committee"
+                  label="Committee"
+                  label-for="exampleInput2">
+        <b-form-select id="exampleInput2"
+                    :options="committeeOptions"
+                    required
+                    v-model="form.committee">
+        </b-form-select>
+      </b-form-group>
 
-        <b-form-group id="date"
-                    label="Date"
-                    label-for="exampleInput3">
-          <b-form-select id="exampleInput3"
-                      :options="dateOptions"
-                      required
-                      v-model="form.date">
-          </b-form-select>
-        </b-form-group>
+      <b-form-group id="date"
+                  label="Date"
+                  label-for="exampleInput3">
+        <b-form-select id="exampleInput3"
+                    :options="dateOptions"
+                    required
+                    v-model="form.date">
+        </b-form-select>
+      </b-form-group>
 
-        <b-form-group id="text"
-                    label="Text"
-                    label-for="exampleInput4">
-          <b-form-select id="exampleInput4"
-                      :options="textOptions"
-                      required
-                      v-model="form.text">
-          </b-form-select>
-        </b-form-group>
+      <b-form-group id="text"
+                  label="Text"
+                  label-for="exampleInput4">
+        <b-form-select id="exampleInput4"
+                    :options="textOptions"
+                    required
+                    v-model="form.text">
+        </b-form-select>
+      </b-form-group>
 
-        <b-form-group id="people"
-                    label="People"
-                    label-for="exampleInput5">
-          <b-form-select id="exampleInput5"
-                      :options="peopleOptions"
-                      required
-                      v-model="form.people">
-          </b-form-select>
-        </b-form-group>
+      <b-form-group id="people"
+                  label="People"
+                  label-for="exampleInput5">
+        <b-form-select id="exampleInput5"
+                    :options="peopleOptions"
+                    required
+                    v-model="form.people">
+        </b-form-select>
+      </b-form-group>
 
-      </div>
+    </div>
   </div>
 </template>
 
