834c513f312cea08cbe1504e8cfddf769768e608
Austin Crapo <corey.crapo@gmail.com>
2018-03-09 10:53:56 -0700

    Fixed the issue with the timeline not appearing, due to time contraints the timestamps were left unfixed and instead are now random

--- transgov/src/prototype/src/components/Timeline.vue
+++ transgov/src/prototype/src/components/Timeline.vue
@@ -12,6 +12,12 @@ var items = {}
 var options = {}
 var timeline = {}
 
+// Temporarily using this function for demo purposes
+// Credit to: https://stackoverflow.com/questions/9035627/elegant-method-to-generate-array-of-random-dates-within-two-dates
+function randomDate(start, end) {
+    return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+}
+
 export default {
   name: 'app',
   mounted: function () {
@@ -39,6 +45,15 @@ export default {
   watch: {
     results: {
       handler: function() {
+        items.clear() // Prevents duplication of results - potential bottleneck if dynamically adding data to a large result set
+        this.results.forEach(function(item, index) {
+          try{
+            items.add({content: item._source.title, start: randomDate(new Date(2012, 0, 1), new Date()), title: item._source.description})
+          }
+          catch(e){
+            console.log("There was an error adding an item")
+          }
+        })
       },
       deep: true
     }
