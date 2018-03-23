8b0a579f0ba74c7c20aeb7cd1db320a0995eda1c
Aquafina99 <xxiang1@ualberta.ca>
2018-03-23 15:51:08 -0600

    attempting change models based on ingested data

--- transgov/src/main_search/models.py
+++ transgov/src/main_search/models.py
@@ -3,11 +3,56 @@ from django.db import models
 from django.urls.base import reverse
 from rest_framework.reverse import reverse as api_reverse
 
-class Category(models.Model):
+class Subsection(models.Model):
     '''
-    The Category Model which has been added as a foreign key to the Meeting model.
+    The Subsection Model which has been added as a foreign key to the Meeting model.
     '''
-    name = models.CharField(max_length=255, db_index=True, unique=True)
+    Title = models.CharField(db_index=True, unique=True)
+    Items =
+
+class Item(models.Model):
+    index_in_pdf =
+    separate_index =
+    title =
+    pages_start =
+    pages_end =
+    text =
+    presenters =
+    motions =
+    keywords = models.CharField(null=True)
+    discussion = models.TextField(db_index=True)
+    purpose =
+
+class Motions(models.Model):
+    motion =
+    carried =
+    content =
+
+class Presenters(models.Model):
+    name =
+
+     "subsection": [
+              { "Title": null,
+                "Items": [
+                   {
+                          "index_in_pdf": null,
+                          "separate_index":null,
+                          "title":null,
+                          "pages_start":null,
+                          "pages_end":null,
+                          "text":null,
+                          "presenters": [ { "name":null } ],
+                          "motions": [
+                            {
+                                "motion":null,
+                                "carried":null,
+                                "content":null
+                            }],
+                          "keywords": [ null, null ],
+                          "discussion":null,
+                          "purpose":null
+                    }]
+              }]
 
     def __str__(self):
         return self.name
@@ -17,12 +62,19 @@ class Meeting(models.Model):
     '''
     The Meeting Model with its essential fields of the data.
     '''
+    filename = models.CharField(db_index=True, default='')
     title = models.CharField(unique=True, max_length=255, db_index=True, default='')
+    date = models.CharField(db_index=True, default='')
+    committee = models.CharField(db_index=True, max_length=255, default='')
+    time = models.CharField(db_index=True, default='')
+    location = models.CharField(db_index=True, default='')
+    attendees = model.TextField(db_index=True)
+    subsection = model.TextField(db_index=True)
     slug = models.SlugField(null=True, blank=True, unique=True, max_length=255)
     description = models.TextField(db_index=True)
     committee = models.CharField(db_index=True, max_length=255)
     category = models.ForeignKey('Category', related_name='category')
-    timestamp = models.DateTimeField(auto_now=True)
+
 
     def __str__(self):
         return self.title
