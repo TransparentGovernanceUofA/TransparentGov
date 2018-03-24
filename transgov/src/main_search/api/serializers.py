56701c69b402e4008e80bd7b8c503523798b1bc7
Aquafina99 <xxiang1@ualberta.ca>
2018-03-24 00:00:21 -0600

    updated on serializer for nested relationships
    Co-authored-by: jbbela <belardo@ualberta.ca>

--- transgov/src/main_search/api/serializers.py
+++ transgov/src/main_search/api/serializers.py
@@ -5,26 +5,26 @@ from main_search.models import Motions, Presenters, Items, Subsection, Meeting
 class MotionsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Motions
-        fields = [
+        fields = (
             'motion',
-            'carries',
+            'carried',
             'content',
-        ]
+        )
 
 class PresentersSerializer(serializers.ModelSerializer):
     class Meta:
         model = Presenters
-        fields = [
+        fields = (
             'name',
-        ]
+        )
 
 class ItemsSerializer(serializers.ModelSerializer):
-    presenters = PresentersSerializer(many=True, read_only=True)
-    motions = MotionsSerializer(many=True, read_only=True)
+    presenters = PresentersSerializer(many=True)
+    motions = MotionsSerializer(many=True)
 
     class Meta:
         model = Items
-        fields = [
+        fields = (
             'index_in_pdf',
             'separate_index',
             'title',
@@ -36,16 +36,31 @@ class ItemsSerializer(serializers.ModelSerializer):
             'keywords',
             'discussion',
             'purpose'
-        ]
+        )
+    def create(self, validated_data):
+        presenter_data = validated_data.pop('name')
+        motion_data = validated_data.pop('motion')
+        presenter = PresentersSerializer.create(PresentersSerializer(), validated_data=presenter_data)
+        motion = MotionsSerializer.create(MotionsSerializer(), validated_data=motion_data)
+        p, created = Presenters.objects.update_or_create(name=name)
+        m, created = Motions.objects.update_or_create(motion=motion,
+                            carried=validated_data.pop('carried'))
+        return i
+
 
 class SubsectionSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Motions
-        fields = [
-            'motion',
-            'carried',
-            'content',
-        ]
+        model = Subsection
+        fields = (
+            'Title',
+            'Items',
+        )
+    def create(self, validated_data):
+        item_data = validated_data.pop('title')
+        item = ItemsSerializer.create(ItemsSerializer(), validated_data=item_data)
+        i, created = Items.objects.update_or_create(title=title,
+                            presenters=validated_data.pop('presenters'))
+        return i
 
 class MeetingSerializer(serializers.ModelSerializer):
     '''
@@ -55,11 +70,11 @@ class MeetingSerializer(serializers.ModelSerializer):
     get_url method:
     returns a full url for the meeting object. Request is passed from MeetingRUDView.
     '''
-    subsection = SubsectionSerializer(many=True, read_only=True)
+    subsection = SubsectionSerializer(many=True)
     url = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = Meeting
-        fields = [
+        fields = (
             'url',
             'pk',
             'filename',
@@ -70,8 +85,15 @@ class MeetingSerializer(serializers.ModelSerializer):
             'location',
             'attendees',
             'subsection',
-        ]
+        )
         read_only_fields = ['pk']
+
+    def create(self, validated_data):
+        subsection_data = validated_data.pop('Title')
+        subsection = SubsectionSerializer.create(SubsectionSerializer(), validated_data=subsection_data)
+        sub, created = Subsection.objects.update_or_create(Title=Title,
+                            Items=validated_data.pop('Items'))
+        return sub
 
     def get_url(self, obj):
         request = self.context.get("request")
