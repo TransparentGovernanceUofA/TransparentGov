a1a3966831be219a3548689817a0822ad30bbc8e
Aquafina99 <xxiang1@ualberta.ca>
2018-05-11 15:54:38 -0600

    added auto request

--- forTASHA.py
+++ forTASHA.py
@@ -5,9 +5,18 @@ from tika import parser
 import os, glob
 import json
 import pathlib
+import requests
+# import config as cfg
 
 
 def adding_TASHA_fields():
+
+    # usr = cfg.tasha['user']
+    # psw = cfg.tasha['passwd']
+
+    usr = input("enter user name: ")
+    psw = input("enter psw: ")
+
     path_to_data = 'paird_material_minutes/bulk_input'
 
 
@@ -26,18 +35,21 @@ def adding_TASHA_fields():
 
                 tika_dict = {'content': new_value, "collection title":committee}
 
-
             with open(file_name+'.txt') as json_file:
                 json_file.seek(0)
                 json_data = json.load(json_file)
                 json_data.update(tika_dict)
 
 
+                payload = {usr:psw}
+                r = requests.post("http://162.246.157.115/tasha/opengovtest", data = json_data, params=payload)
+                print(r)
 
-            json_filename = "DATA/" + file_name+'.txt'
-            os.makedirs(os.path.dirname(json_filename), exist_ok=True)
-            with open(json_filename, "w") as outfile:
-                json.dump(json_data, outfile, indent=4, ensure_ascii=False)
+
+            # json_filename = "DATA/" + file_name+'.txt'
+            # os.makedirs(os.path.dirname(json_filename), exist_ok=True)
+            # with open(json_filename, "w") as outfile:
+            #     json.dump(json_data, outfile, indent=4, ensure_ascii=False)
 
 
 adding_TASHA_fields()
